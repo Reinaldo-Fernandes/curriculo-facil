c8a787065717cf0a48e6b09b3c9ee7e284b98811
REINALDO FERNANDES <reinaldo.f.menezes@outlook.com>
2024-10-26 17:40:32 -0300

    Update script.js
    Removendo o elemento projetos
    Contador de caracteres do resumo
    fazendo com que os elementos habilidades e Idiomas fiquem na coluna dos dados pessoais e que apresente o que foi digitado um abaixo do outro

--- script.js
+++ script.js
@@ -29,6 +29,19 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    // Contador para o campo "Resumo"
+    document.getElementById('summary').addEventListener('input', function () {
+        const summary = document.getElementById('summary');
+        const counter = document.getElementById('summaryCounter');
+        const maxLength = 500;
+        const currentLength = summary.value.length;
+
+        if (currentLength > maxLength) {
+            summary.value = summary.value.substring(0, maxLength);
+        }
+        counter.textContent = `${summary.value.length} / ${maxLength} caracteres`;
+    });
+
     // Exibe o texto do tooltip dinamicamente
     document.querySelectorAll('.info-card').forEach(card => {
         const tooltip = card.querySelector('.tooltip');
@@ -37,7 +50,6 @@ document.addEventListener('DOMContentLoaded', function () {
             tooltip.textContent = text;
         }
     });
-
 
     // Adicionar seções dinâmicas
     addSection('addEducation', 'educationContainer', `
@@ -57,14 +69,6 @@ document.addEventListener('DOMContentLoaded', function () {
             <button type="button" class="remove-button">Remover</button>
         </div>
     `);
-    addSection('addProject', 'projectsContainer', `
-        <div class="project-entry">
-            <input type="text" class="project-name" placeholder="Nome do Projeto">
-            <input type="text" class="project-link" placeholder="Link (se houver)">
-            <textarea class="project-description" placeholder="Descrição breve do projeto."></textarea>
-            <button type="button" class="remove-button">Remover</button>
-        </div>
-    `);
     addSection('addCertification', 'certificationsContainer', `
         <div class="certification-entry">
             <input type="text" class="certification-name" placeholder="Nome da Certificação">
@@ -74,86 +78,101 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
     `);
 
+    // Formatação do Telefone
+    function formatPhone(phoneInput) {
+        phoneInput.addEventListener('input', () => {
+            let phoneValue = phoneInput.value.replace(/\D/g, '');
+            if (phoneValue.length > 10) {
+                phoneInput.value = `(${phoneValue.slice(0, 2)}) ${phoneValue.slice(2, 7)}-${phoneValue.slice(7, 11)}`;
+            } else if (phoneValue.length > 6) {
+                phoneInput.value = `(${phoneValue.slice(0, 2)}) ${phoneValue.slice(2, 6)}-${phoneValue.slice(6)}`;
+            } else if (phoneValue.length > 2) {
+                phoneInput.value = `(${phoneValue.slice(0, 2)}) ${phoneValue.slice(2)}`;
+            }
+        });
+    }
+    formatPhone(document.getElementById('phone1'));
+    formatPhone(document.getElementById('phone2'));
+
     // Função para gerar currículo
     function generateResume() {
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const phone1 = document.getElementById('phone1').value;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone1 = document.getElementById('phone1').value;
 
-        if (!name || !email || !phone1) {
-            alert("Por favor, preencha os campos obrigatórios.");
-            return;
-        }
-
-        const resumeData = {
-            name: name,
-            address: document.getElementById('address').value,
-            phone1: phone1,
-            phone2: document.getElementById('phone2').value,
-            email: email,
-            linkedin: document.getElementById('linkedin').value,
-            summary: document.getElementById('summary').value,
-            education: Array.from(document.querySelectorAll('.education-entry')).map(entry => ({
-                title: entry.querySelector('.education-title').value,
-                institution: entry.querySelector('.education-institution').value,
-                duration: entry.querySelector('.education-duration').value,
-            })),
-            experience: Array.from(document.querySelectorAll('.experience-entry')).map(entry => ({
-                title: entry.querySelector('.experience-title').value,
-                company: entry.querySelector('.experience-company').value,
-                duration: entry.querySelector('.experience-duration').value,
-                description: entry.querySelector('.experience-description').value,
-            })),
-            skills: document.getElementById('skills').value,
-            projects: Array.from(document.querySelectorAll('.project-entry')).map(entry => ({
-                name: entry.querySelector('.project-name').value,
-                link: entry.querySelector('.project-link').value,
-                description: entry.querySelector('.project-description').value,
-            })),
-            certifications: Array.from(document.querySelectorAll('.certification-entry')).map(entry => ({
-                name: entry.querySelector('.certification-name').value,
-                institution: entry.querySelector('.certification-institution').value,
-                description: entry.querySelector('.certification-description').value,
-            })),
-            languages: document.getElementById('languages').value,
-            activities: document.getElementById('activities').value,
-            photo: photoURL // Usa a variável photoURL corretamente agora
-        };
-
-        displayResumePreview(resumeData);
-        resumePreview.style.display = 'flex'; // Exibe o preview
+    if (!name || !email || !phone1) {
+        alert("Por favor, preencha os campos obrigatórios.");
+        return;
     }
+
+    const resumeData = {
+        name: name,
+        address: document.getElementById('address').value,
+        phone1: phone1,
+        phone2: document.getElementById('phone2').value,
+        email: email,
+        linkedin: document.getElementById('linkedin').value,
+        summary: document.getElementById('summary').value,
+        education: Array.from(document.querySelectorAll('.education-entry')).map(entry => ({
+            title: entry.querySelector('.education-title').value,
+            institution: entry.querySelector('.education-institution').value,
+            duration: entry.querySelector('.education-duration').value,
+        })),
+        experience: Array.from(document.querySelectorAll('.experience-entry')).map(entry => ({
+            title: entry.querySelector('.experience-title').value,
+            company: entry.querySelector('.experience-company').value,
+            duration: entry.querySelector('.experience-duration').value,
+            description: entry.querySelector('.experience-description').value,
+        })),
+        certifications: Array.from(document.querySelectorAll('.certification-entry')).map(entry => ({
+            name: entry.querySelector('.certification-name').value,
+            institution: entry.querySelector('.certification-institution').value,
+            description: entry.querySelector('.certification-description').value,
+        })),
+        skills: document.getElementById('skills').value.split(',').map(skill => skill.trim()).filter(skill => skill),
+        languages: document.getElementById('languages').value.split(',').map(lang => lang.trim()).filter(lang => lang),
+        activities: document.getElementById('activities').value,
+        photo: photoURL
+    };
+
+    displayResumePreview(resumeData);
+    resumePreview.style.display = 'flex';
+}
+
 
     // Função para exibir o preview do currículo
     function displayResumePreview(data) {
-    resumePreview.innerHTML = `
-        <div class="resume-left custom-bg-color">
-            ${data.photo ? `<img src="${data.photo}" alt="Foto">` : ''}
-            <h2>${data.name}</h2>
-            <div class="contact-info">
-                ${data.address ? `<p><strong>Endereço:</strong> ${data.address}</p>` : ''}
-                <p><strong>Telefone 1:</strong> ${data.phone1}</p>
-                ${data.phone2 ? `<p><strong>Telefone 2:</strong> ${data.phone2}</p>` : ''}
-                <p><strong>Email:</strong> ${data.email}</p>
-                ${data.linkedin ? `<p><strong>LinkedIn:</strong> ${data.linkedin}</p>` : ''}
+        const skillsHTML = data.skills && data.skills.length ? `<h3>Habilidades</h3><ul class="skills-display">${data.skills.map(skill => `<li>${skill}</li>`).join('')}</ul>` : '';
+        const languagesHTML = data.languages && data.languages.length ? `<h3>Idiomas</h3><ul class="languages-display">${data.languages.map(lang => `<li>${lang}</li>`).join('')}</ul>` : '';
+    
+        resumePreview.innerHTML = `
+            <div class="resume-left custom-bg-color">
+                ${data.photo ? `<img src="${data.photo}" alt="Foto">` : ''}
+                <h2>${data.name}</h2>
+                <div class="contact-info">
+                    ${data.address ? `<p><strong>Endereço:</strong> ${data.address}</p>` : ''}
+                    <p><strong>Telefone 1:</strong> ${data.phone1}</p>
+                    ${data.phone2 ? `<p><strong>Telefone 2:</strong> ${data.phone2}</p>` : ''}
+                    <p><strong>Email:</strong> ${data.email}</p>
+                    ${data.linkedin ? `<p><strong>LinkedIn:</strong> ${data.linkedin}</p>` : ''}
+                    ${skillsHTML}
+                    ${languagesHTML}
+                </div>
             </div>
-            ${data.skills ? `<div class="skills"><h3>Habilidades</h3><p>${data.skills}</p></div>` : ''}
-        </div>
-        <div class="resume-right">
-            ${data.summary ? `<div class="summary"><h3>Resumo</h3><p>${data.summary}</p></div>` : ''}
-            ${data.education.length ? `<h3>Educação</h3><ul>${data.education.map(edu => `<li>${edu.title} - ${edu.institution} (${edu.duration})</li>`).join('')}</ul>` : ''}
-            ${data.experience.length ? `<h3>Experiência Profissional</h3><ul>${data.experience.map(exp => `<li><strong>${exp.title}</strong> - ${exp.company} (${exp.duration})<br>${exp.description}</li>`).join('')}</ul>` : ''}
-            ${data.projects.length ? `<h3>Projetos</h3><ul>${data.projects.map(proj => `<li><strong>${proj.name}</strong> ${proj.link ? `- <a href="${proj.link}" target="_blank">Link</a>` : ''}<br>${proj.description}</li>`).join('')}</ul>` : ''}
-            ${data.certifications.length ? `<h3>Certificações</h3><ul>${data.certifications.map(cert => `<li><strong>${cert.name}</strong> - ${cert.institution}<br>${cert.description}</li>`).join('')}</ul>` : ''}
-            ${data.languages ? `<h3>Idiomas</h3><p>${data.languages}</p>` : ''}
-            ${data.activities ? `<h3>Atividades Extracurriculares</h3><p>${data.activities}</p>` : ''}
-        </div>
-    `;
-}
+            <div class="resume-right">
+                ${data.summary ? `<div class="summary"><h3>Resumo</h3><p>${data.summary}</p></div>` : ''}
+                ${data.education && data.education.length ? `<h3>Educação</h3><ul>${data.education.map(edu => `<li>${edu.title} - ${edu.institution} (${edu.duration})</li>`).join('')}</ul>` : ''}
+                ${data.experience && data.experience.length ? `<h3>Experiência Profissional</h3><ul>${data.experience.map(exp => `<li><strong>${exp.title}</strong> - ${exp.company} (${exp.duration})<br>${exp.description}</li>`).join('')}</ul>` : ''}
+                ${data.certifications && data.certifications.length ? `<h3>Certificações</h3><ul>${data.certifications.map(cert => `<li><strong>${cert.name}</strong> - ${cert.institution}<br>${cert.description}</li>`).join('')}</ul>` : ''}
+                ${data.activities ? `<h3>Atividades Extracurriculares</h3><p>${data.activities}</p>` : ''}
+            </div>
+        `;
+    }
+    
 
     // Função para baixar currículo como PDF
     document.getElementById('downloadPdf').addEventListener('click', function (event) {
-        event.preventDefault(); // Impede o comportamento padrão do botão
+        event.preventDefault();
         const { jsPDF } = window.jspdf;
         const doc = new jsPDF({
             orientation: 'portrait',
@@ -164,20 +183,16 @@ document.addEventListener('DOMContentLoaded', function () {
         const resumePreview = document.getElementById('resumePreview');
         html2canvas(resumePreview, { scale: 3, useCORS: true }).then(canvas => {
             const imgData = canvas.toDataURL('image/png');
-            const imgWidth = 210; // Largura A4 em mm
-            const pageHeight = 297; // Altura A4 em mm
+            const imgWidth = 210;
             const imgHeight = canvas.height * imgWidth / canvas.width;
-            let position = 0;
-
-            doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-
-            doc.save('curriculo.pdf'); // Salva o PDF
+            doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+            doc.save('curriculo.pdf');
         });
     });
 
     // Listener para o botão "Gerar Currículo"
     document.getElementById('generateResumeButton').addEventListener('click', function (event) {
-        event.preventDefault(); // Impede o comportamento padrão de envio
-        generateResume(); // Chama a função de gerar o currículo
+        event.preventDefault();
+        generateResume();
     });
 });
